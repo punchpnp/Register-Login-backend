611222ab9a390085c96c38dddd157473601abf2b
punchpnp <nunnapatppp@gmail.com>
2024-02-27 18:53:15 +0700

    Add appointments

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,4 +1,5 @@
 const Appointment = require("../models/Appointment");
+const Hospital = require("../models/Hospital");
 
 //@desc  Get all appointments
 //@route Get /api/v1/appointments
@@ -32,5 +33,66 @@ exports.getAppointments = async (req, res, next) => {
     return res
       .status(500)
       .json({ success: false, message: "Cannot find Appointment" });
+  }
+};
+
+//@desc  Get single appointments
+//@route Get /api/v1/appointments/:id
+//@access Public
+exports.getAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id).populate({
+      path: "hospital",
+      select: "name description tel",
+    });
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (err) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Cannot find Appointment" });
+  }
+};
+
+//@desc  Add appointments
+//@route POST /api/v1/hospitals/:hospitalId/appointment
+//@access Private
+exports.addAppointment = async (req, res, next) => {
+  try {
+    req.body.hospital = req.params.hospitalId;
+
+    const hospital = await Hospital.findById(req.params.hospitalId);
+
+    if (!hospital) {
+      return res
+        .status(404)
+        .json({
+          success: false,
+          message: `No hospital with the id of ${req.params.hospitalId}`,
+        });
+    }
+
+    const appointment = await Appointment.create(req.body);
+
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (err) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Cannot create Appointment" });
   }
 };
--- routes/appointments.js
+++ routes/appointments.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getAppointments } = require("../controllers/appointments");
+const { getAppointments , getAppointment, addAppointment} = require("../controllers/appointments");
 
 const router = express.Router({ mergeParams: true });
 
 const { protect } = require("../middleware/auth");
 
-router.route("/").get(protect, getAppointments);
+router.route("/").get(protect, getAppointments).post(addAppointment);
+router.route("/:id").get(getAppointment);
 
 module.exports = router;
